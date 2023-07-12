44a39953e88e262535c133b9f341dde1345e717d
Aba Sargious <abanoub.sargious@estudiantat.upc.edu>
2023-07-12 11:19:42 +0200

    Manual control marker

--- apps/RGS/static/RGS/js/manual_control.js
+++ apps/RGS/static/RGS/js/manual_control.js
@@ -11,7 +11,13 @@ $(document).ready(function() {
     // Function to update the UAV's state every second
     // Initialize the map
     var mymap = L.map('mapid').setView([lat, lon], 30); // center the map to some default coordinates, these should be replaced by your UAV's starting coordinates
-    
+    mymap.setView([lat, lon],16); // re-center the map to the UAV's current location
+    // Define our airplane icon
+    var airplaneIcon = L.icon({
+        iconUrl: "https://www.vhv.rs/dpng/d/215-2150840_transparent-plane-icon-png-airplane-icon-png-png.png", // put the path of your icon image
+        iconSize: [25, 25], // size of the icon
+        iconAnchor: [12.5, 12.5], // point of the icon which will correspond to marker's location
+    });
     // Add a tile layer to the map
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 30,
@@ -19,7 +25,8 @@ $(document).ready(function() {
     
     // Create a new line (polyline) instance. This will hold the path of your UAV.
     var polyline = L.polyline([], {color: 'red'}).addTo(mymap);
-    
+    var marker_plane = L.marker([lat, lon], {icon: airplaneIcon}).addTo(mymap);
+
     function updateState(){
         // AJAX call
         $.ajax({
@@ -36,8 +43,9 @@ $(document).ready(function() {
                 // Now update the map
                 var lat = data.location[0];
                 var lon = data.location[1];
-                mymap.setView([lat, lon], 15); // re-center the map to the UAV's current location
+                mymap.panTo([lat, lon]); // only change the center to the UAV's current location without changing the zoom
                 polyline.addLatLng([lat, lon]); // add the new point to the polyline
+                marker_plane.setLatLng([lat, lon]); // update the marker's position to the UAV's current location
             },
         });
     };
@@ -45,6 +53,10 @@ $(document).ready(function() {
     // Start the periodic state update
     setInterval(updateState, 1000); // update every second
 
+    // Add marker to the map
+    function addMarker(lat, lon) {
+        var marker = L.marker([lat, lon]).addTo(mymap);
+    }
     // Set event handlers for the buttons
     $('.btn-info').click(function(){
         var id = this.id;
